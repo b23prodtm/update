ce4c826e9569f24695569a5975b9b5d6a0ded0a8
Tina B Rakoto <webmaster@b23prodtm.info>
2025-04-16 23:17:25 +0200

    Installer name

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'betothreeprod/updateshell',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '302969b09de23cabe6e17471b680b7476ad90280',
+        'type' => 'cakephp-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'betothreeprod/updateshell' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '302969b09de23cabe6e17471b680b7476ad90280',
+            'type' => 'cakephp-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
